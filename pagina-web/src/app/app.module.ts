f57e2281f241aa93238e546278dd3330268c3ac1
cristian pittoors <cristian.pittoors@gmail.com>
2022-04-30 11:57:36 -0400

    cambio de pagina basico

--- pagina-web/src/app/app.module.ts
+++ pagina-web/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
